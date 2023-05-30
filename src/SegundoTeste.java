89538fd1b0837e7c9e90e0bfa5755323a05943f0
Hitalo Jacome <106604805+hitalojacome@users.noreply.github.com>
2023-05-30 10:37:02 -0300

    Update SegundoTeste.java

--- src/SegundoTeste.java
+++ src/SegundoTeste.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class SegundoTeste {
+import java.util.Scanner;
+
+public class SegundoTeste {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String nome;
+        int idade;
+        String formacao;
+        String cargo;
+
+        System.out.println("Meu segundo pgm em Java!");
+
+        System.out.println("Informe seu nome");
+        nome = scanner.nextLine();
+
+        System.out.println("Insira sua idade");
+        idade = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Qual sua formação?");
+        formacao = scanner.nextLine();
+
+        System.out.println("E qual seu papel no demoday?");
+        cargo = scanner.nextLine();
+
+        System.out.println("Ola " + nome + ", você tem " + idade + " anos");
+        System.out.println("É formado em " + formacao + " e cumpre o papel de " + cargo + " em seu demoday." );
+    }
 }
