8fe271ecfbc02222c9c3d140f4bdb2acea8e1ece
Hitalo Jacome <106604805+hitalojacome@users.noreply.github.com>
2023-05-30 10:36:34 -0300

    Update PrimeiroTeste.java

--- src/PrimeiroTeste.java
+++ src/PrimeiroTeste.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class PrimeiroTeste {
+class PrimeiroTeste {
+
+    public static void main (String args[]){
+
+        System.out.println("Hitalo Jacome");
+        System.out.println("19 anos");
+        System.out.println("Formacao full stack Instituto Proa");
+        System.out.println("Faz parte da equipe do back end e responsavel pelo banco de dados");
+    }
 }
